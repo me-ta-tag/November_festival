ee65e643914464bfa098be7fd8704403eadf4b4e
Kentaro Hagiwara <rastyrein.ken.091012@gmail.com>
2014-08-30 01:51:26 +0900

    logintest

--- app/Controller/ShopsController.php
+++ app/Controller/ShopsController.php
@@ -6,8 +6,54 @@
  * Time: 16:00
  */
 
+App::uses('AppController', 'Controller');
+
 class ShopsController extends AppController {
 //    public $scaffold;
+
+    //読み込むコンポーネントの指定
+    public $components = array('Session', 'Auth');
+
+    //どのアクションが呼ばれてもはじめに実行される関数
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+
+        //未ログインでアクセスできるアクションを指定
+        //これ以外のアクションへのアクセスはloginにリダイレクトされる規約になっている
+        $this->Auth->allow('register', 'login');
+    }
+
+    //ログイン後にリダイレクトされるアクション
+    public function index(){
+        $this->set('user', $this->Auth->user());
+    }
+
+    public function register(){
+        //$this->requestにPOSTされたデータが入っている
+        //POSTメソッドかつユーザ追加が成功したら
+        if($this->request->is('post') && $this->User->save($this->request->data)){
+            //ログイン
+            //$this->request->dataの値を使用してログインする規約になっている
+            $this->Auth->login();
+            $this->redirect('index');
+        }
+    }
+
+    public function login(){
+        if($this->request->is('post')) {
+            if($this->Auth->login())
+                return $this->redirect('index');
+            else
+                $this->Session->setFlash('ログイン失敗');
+        }
+    }
+
+    public function logout(){
+        $this->Auth->logout();
+        $this->redirect('login');
+    }
+    /*
     public $helpers = array('Html','Form');
 
     public function index(){
@@ -43,4 +89,5 @@ class ShopsController extends AppController {
             //$this->Session->setFlash('error');
         }
     }
+    */
 }
