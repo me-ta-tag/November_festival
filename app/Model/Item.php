74f4dc548cada042bcf2f67a26fddbdb9df6fb8d
Kentaro Hagiwara <rastyrein.ken.091012@gmail.com>
2014-09-16 20:40:23 +0900

    fileuploadPlugin Add

--- app/Model/Item.php
+++ app/Model/Item.php
@@ -9,9 +9,10 @@
 App::uses('AppModel', 'Model');
 class Item extends AppModel{
     var $name = 'Item';
+
     var $belongsTo = array(
-        'Category' => array (               // ここから追加
-            'className' => 'Category',
+    'Category' => array (               // ここから追加
+        'className' => 'Category',
             'conditions' => 'Item.category_id = Category.id',
             'order' => 'Item.id ASC',
             'foreignKey' => ''
