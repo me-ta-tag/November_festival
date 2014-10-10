3c1222ab56c6038a307849e69b87cf0a7656d57b
Kentaro Hagiwara <rastyrein.ken.091012@gmail.com>
2014-10-10 23:50:17 +0900

    [] replase array()

--- app/Model/Item.php
+++ app/Model/Item.php
@@ -10,26 +10,25 @@ App::uses('AppModel', 'Model');
 class Item extends AppModel{
     var $name = 'Item';
 
-    var $validate = [
-        'item_name' => [
-            'notempty' => [
-                'rule' => ['notempty'],
-            ]
-        ],
-        'item_price' => [
-            'notempty' => [
-                'rule' => ['notempty'],
-            ]
-        ],
-        'item_stock' => [
-            'notempty' => [
-                'rule' => ['notempty'],
-            ]
-        ],
-        'item_photo' =>[
-        ]
+    var $validate = array(
+        'item_name' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            )
+        ),
+        'item_price' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            )
+        ),
+        'item_stock' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            )
+        ),
+        'item_photo' =>array()
 
-    ];
+    );
     //$conditions = array('NOT' => array('item_name' => null,'item_price' => null);
     var $belongsTo = array(
     'Category' => array (               // ここから追加
@@ -39,20 +38,20 @@ class Item extends AppModel{
             'foreignKey' => ''
         )
     );
-    public $actsAs = [
-        'Upload.Upload' => [
-            'item_photo' => [
-                'fields' => [
+    public $actsAs = array(
+        'Upload.Upload' => array(
+            'item_photo' => array(
+                'fields' => array(
                     'dir' => 'item_photo_dir',
                     'type' => 'type',
                     'size' => 'size'
-                ],
-                'thumbnailSizes' => [
+                ),
+                'thumbnailSizes' => array(
                     'thumb150' => '150x150',
                     'normal' => '400x400',
                     'longBig' => '960x350'
-                ],
-                'extensions' => [
+                ),
+                'extensions' => array(
                     'jpg',
                     'jpeg',
                     'JPG',
@@ -61,13 +60,13 @@ class Item extends AppModel{
                     'GIF',
                     'png',
                     'PNG'
-                ],
-                'mimetypes' => ['image/jpeg', 'image/gif', 'image/png'],
+                ),
+                'mimetypes' => array('image/jpeg', 'image/gif', 'image/png'),
                 'path' => 'webroot/img{DS}{model}{DS}{field}{DS}',
                 'thumbnailMethod' => 'php'
-            ]
-        ]
-    ];
+            )
+        )
+    );
     //                'path' => '{ROOT}tmp{DS}files{DS}{model}{DS}{field}{DS}',
     //'path' => '{ROOT}img{DS}{model}{DS}{field}{DS}',
 //    public $actsAs = [
