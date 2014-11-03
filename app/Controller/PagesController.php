62af131c797c6bb4b3a61eee485514ccae2a4bff
Kentaro Hagiwara <rastyrein.ken.091012@gmail.com>
2014-11-03 15:10:13 +0900

    metareadの呼び出しを変更しました

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -35,8 +35,9 @@ class PagesController extends AppController {
  *
  * @var array
  */
-	public $uses = array();
-
+	//public $uses = array();
+    var $uses = array('Item', 'Category','Sale');
+    public $components = array('RequestHandler');
 /**
  * Displays a view
  *
@@ -74,4 +75,57 @@ class PagesController extends AppController {
 			throw new NotFoundException();
 		}
 	}
+
+
+
+    public function metaread(){
+        //response.setHeader("Access-Control-Allow-Origin","*");
+        //if($this->request->is('ajax')) {
+        if ($this->request->is('get')) {
+            $params = array(
+                //'fields' => array( compact('item_exhibitor')),
+                'conditions' => array('Item.shop_id' => 1,
+                    'AND' => array(
+                        'NOT' => array(
+                            'Item.item_photo' => null
+                        )
+                    )
+                ),
+                'order' => 'Item.id DESC'
+            );
+            // 'fields' => array('id','item_name','item_price','item_detail','item_photo','item_photo_dir','item_stock','item_leader','category_id','category_name'),
+            $pdo = $this->Item->getDatasource()->getConnection();
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,FALSE);
+            $items = $this->Item->find('all', $params);
+
+            $salparams = array(
+                'shop_id' => array('shop_id' => 1)
+            );
+            $sales = $this->Sale->find('all', $salparams);
+
+            $salesArray = array();
+            foreach($sales as $key => $value){
+                $salesArray[$value['Sale']['item_id']] = $value['Sale']['sale_quantity'] + $salesArray[$value['Sale']['item_id']];
+            }
+            //var_dump($salesArray);
+            foreach ($items as $key => $value) {
+                $items[$key]["Item"]["item_stock"] = $items[$key]["Item"]["item_stock"] - $salesArray[$items[$key]["Item"]["id"]];
+                unset($items[$key]["Item"]["tweet"],$items[$key]["Item"]["item_exhibitor"],$items[$key]["Item"]["shop_id"],$items[$key]["Category"]["shop_id"]);
+            }
+
+            // viewにはjson形式のファイルを表示させるように。
+            $this->layout = 'ajax';
+            $this->RequestHandler->setContent('json');
+            $this->RequestHandler->respondAs('application/json; charset=UTF-8');
+
+            // $studentsの配列をviewに渡す。
+            $this->set('items', $items);
+        } else {
+            throw new NotFoundException();
+        }
+        //}else{
+        //  throw new NotFoundException();
+        //}
+    }
+
 }
--- app/View/Users/register.ctp
+++
@@ -1,7 +0,0 @@
-<h1>新規登録</h1>
-<?php print(
-  $this->Form->create('User') .
-$this->Form->input('username') .
-$this->Form->input('password') .
-$this->Form->end('Submit')
-); ?>
